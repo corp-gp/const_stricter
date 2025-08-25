15e4ceee89e927074e8dbe7edac53955f2f9ffe0
Sergei Malykh <xronos.i.am@gmail.com>
2025-08-25 16:01:10 +0300

    init repo

---
+++ lib/constrictor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Constrictor
+  VERSION = "0.1.0"
+end
---
+++ spec/constrictor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Constrictor do
+  it "has a version number" do
+    expect(Constrictor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
